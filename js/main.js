3f40866da1f5bb025d2e69c844991b7d40a259a9
Troy Dorman <troydorman@gmail.com>
2021-08-28 19:42:13 -0600

    incorporated sample pdf for project.  using pdf.js we read the document

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-const url = '../docs/pdf.pdf';
+const url = "../docs/pdf.pdf";
 
 let pdfDoc = null,
-    pageNum = 1,
-    pageIsRendering = false,
-    pageNumIsPending = null;
-
+  pageNum = 1,
+  pageIsRendering = false,
+  pageNumIsPending = null;
 
 const scale = 1.5,
-    canvas = document.querySelector('#pdf-render'),
-    ctx = canvas.getContext('2d');
+  canvas = document.querySelector("#pdf-render"),
+  ctx = canvas.getContext("2d");
 
-// 
+// Render the page
+const renderPage = (num) => {};
+
+// get document
+pdfjsLib.getDocument(url).promise.then((pdfDoc_) => {
+  pdfDoc = pdfDoc_;
+  console.log(pdfDoc);
+});
